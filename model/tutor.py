866cb1c283bd6b3d1dd8bfede5a8f2bd0cc52521
ebeket <probikeparkrider@gmail.com>
2025-03-28 22:14:31 -0700

    Add color coding to topics

--- model/tutor.py
+++ model/tutor.py
@@ -18,19 +18,19 @@ class TutorContent(BaseModel):
 
 sample_data = TutorContent(subjects=[
     Subject(name="Pre-Algebra", topics=[
-        Topic(name="Integers", proficiency=9),
-        Topic(name="Fractions", proficiency=9),
-        Topic(name="Decimals", proficiency=9),
+        Topic(name="Integers", proficiency=100),
+        Topic(name="Fractions", proficiency=25),
+        Topic(name="Decimals", proficiency=67),
     ]),
     Subject(name="Algebra", topics=[
-        Topic(name="Linear Equations", proficiency=5),
-        Topic(name="Quadratic Equations", proficiency=4),
+        Topic(name="Linear Equations", proficiency=99),
+        Topic(name="Quadratic Equations", proficiency=43),
         Topic(name="Exponents", proficiency=3),
     ]),
     Subject(name="Geometry", topics=[
-        Topic(name="Pythagorean Theorem", proficiency=0),
+        Topic(name="Pythagorean Theorem", proficiency=12),
         Topic(name="Angles", proficiency=0),
-        Topic(name="Circles", proficiency=0),
+        Topic(name="Circles", proficiency=6),
     ]),
 ])
 
