efacc4c81887e8ece91b1688447f803dbeef5611
Yason Khaburzaniya <yason@hey.com>
2025-04-02 14:16:39 -0700

    remove obsolete code

--- model/tutor.py
+++ model/tutor.py
@@ -32,31 +32,3 @@ class TutorContent(BaseModel):
             subject.topics[topic_name] = topic
 
         return subject.topics.get(topic_name)
-
-
-sample_data = TutorContent(subjects={
-    "Pre-Algebra": Subject(
-        name="Pre-Algebra",
-        topics={
-            "Integers": Topic(name="Integers", level=10),
-            "Fractions": Topic(name="Fractions", level=3),
-            "Decimals": Topic(name="Decimals", level=7),
-        }
-    ),
-    "Algebra": Subject(
-        name="Algebra",
-        topics={
-            "Linear Equations": Topic(name="Linear Equations", level=10),
-            "Quadratic Equations": Topic(name="Quadratic Equations", level=5),
-            "Exponents": Topic(name="Exponents", level=1),
-        }
-    ),
-    "Geometry": Subject(
-        name="Geometry",
-        topics={
-            "Pythagorean Theorem": Topic(name="Pythagorean Theorem", level=2),
-            "Angles": Topic(name="Angles", level=1),
-            "Circles": Topic(name="Circles", level=1),
-        }
-    ),
-})
